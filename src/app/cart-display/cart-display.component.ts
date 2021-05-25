174f59b5a99abd3a9245b23becf0e4944a1be2ac
Ferdinand Malcher <ferdinand@malcher.media>
2021-05-25 19:49:08 +0200

    CartDisplayComponent hinzugefügt

---
+++ src/app/cart-display/cart-display.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'br-cart-display',
+  templateUrl: './cart-display.component.html'
+})
+export class CartDisplayComponent {
+
+  @Input() books: Book[] = [];
+
+  get totalValue(): number {
+    return this.books.reduce((acc, book) => acc + book.price, 0);
+  }
+
+}
