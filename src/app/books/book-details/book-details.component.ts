98b48f04dbdf8e2d11ce5c3c45fd4d8c97d78c8a
Ferdinand Malcher <ferdinand@malcher.media>
2022-01-11 11:44:54 +0100

    Detailseite mit RxJS

--- src/app/books/book-details/book-details.component.ts
+++ src/app/books/book-details/book-details.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { EMPTY, map, Observable, switchMap } from 'rxjs';
+import { EMPTY, map, Observable, switchAll, switchMap } from 'rxjs';
 import { Book } from '../shared/book';
 import { BookStoreService } from '../shared/book-store.service';
 
@@ -14,17 +14,22 @@ export class BookDetailsComponent {
   book$: Observable<Book>;
 
   constructor(private route: ActivatedRoute, private bs: BookStoreService) {
-    this.book$ = EMPTY; // TODO
-
     // Synchroner Weg (PULL)
     // const isbn = this.route.snapshot.paramMap.get('isbn'); // /books/details/:isbn
     // console.log('ISBN', isbn);
 
     // Asynchroner Weg (PUSH)
-    this.route.paramMap.subscribe(params => {
-      const isbn = params.get('isbn')!; // Non-Null Assertion
-      this.book$ = this.bs.getSingle(isbn);
-    });
+    this.book$ = this.route.paramMap.pipe(
+      map(params => params.get('isbn')!),
+      switchMap(isbn => this.bs.getSingle(isbn))
+    );
+
+
+
+
+
+
+
   }
 
 }
