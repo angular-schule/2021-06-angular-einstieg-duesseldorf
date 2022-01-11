af626ab0aaa4fc6550aeca37c93cf12de57b769e
Ferdinand Malcher <ferdinand@malcher.media>
2022-01-11 09:46:47 +0100

    Redirect

--- src/app/books/books-routing.module.ts
+++ src/app/books/books-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: 'books', redirectTo: 'books/list', pathMatch: 'full' },
   { path: 'books/list', component: DashboardComponent },
   { path: 'books/details/:isbn', component: BookDetailsComponent },
 ];
